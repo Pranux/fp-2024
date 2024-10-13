7d0296bb507bfeaacbd964d068745502044a366d
Justinas P <justinas.pranaitis@mif.stud.vu.lt>
2024-10-13 23:46:04 +0300

    Initial commit for Lab2

--- src/Lib2.hs
+++ src/Lib2.hs
@@ -1,44 +1,222 @@
-{-# LANGUAGE InstanceSigs #-}
+
 module Lib2
     ( Query(..),
     parseQuery,
     State(..),
     emptyState,
-    stateTransition
+    stateTransition,
+    or2,
+    parseAddMovie,
+    parseRemoveMovie,
+    parseCompoundQuery,
+    parseNum,
+    wordsBy,
+    addMovieTransition,
+    removeMovieTransition,
+    printMoviesTransition,
+    removeMovie,
+    addMovie,
+    printMovie,
+    printMovies,
+    Movie(director, title, year, id)
     ) where
 
--- | An entity which represets user input.
--- It should match the grammar from Laboratory work #1.
--- Currently it has no constructors but you can introduce
--- as many as needed.
-data Query
+import Data.Char as C (isDigit)
 
--- | The instances are needed basically for tests
+-- main data type
+data Movie = Movie {
+    director :: String,
+    title :: String,
+    year :: Integer,
+    id :: Integer
+    } deriving (Show, Eq)
+
+data Query =  AddMovie String String Integer Integer
+            | RemoveMovie Integer
+            | PrintMovies
+            | CompoundQuery Query Query
+
+-- The instances are needed basically for tests
 instance Eq Query where
-  (==) _ _= False
+    (AddMovie dir1 title1 year1 id1) == (AddMovie dir2 title2 year2 id2) =
+        dir1 == dir2 && title1 == title2 && year1 == year2 && id1 == id2
+
+    (RemoveMovie idNum1) == (RemoveMovie idNum2) =
+        idNum1 == idNum2
+
+    PrintMovies == PrintMovies = True
+
+    (CompoundQuery q1a q1b) == (CompoundQuery q2a q2b) =
+        q1a == q2a && q1b == q2b
+
+    _ == _ = False
 
 instance Show Query where
-  show _ = ""
+    show (AddMovie d t y i) =
+        "AddMovie: Director = " ++ d ++ ", Title = " ++ t ++ ", Year = " ++ show y ++ ", ID = " ++ show i
+    show (RemoveMovie idNum) =
+        "RemoveMovie: ID = " ++ show idNum
+    show PrintMovies = printMovies movieList
+    show (CompoundQuery q1 q2 ) = show q1 ++ " & " ++ show q2
 
--- | Parses user's input.
--- The function must have tests.
-parseQuery :: String -> Either String Query
-parseQuery _ = Left "Not implemented 2"
+-- Define the Parser type
+type Parser a = String -> Either String (a, String)
 
--- | An entity which represents your program's state.
--- Currently it has no constructors but you can introduce
--- as many as needed.
-data State
+-- An entity which represents your program's state.
+data State = State [Movie] deriving Show
 
--- | Creates an initial program's state.
--- It is called once when the program starts.
+-- Creates an initial program's state.
 emptyState :: State
-emptyState = error "Not implemented 1"
+emptyState = State []
 
--- | Updates a state according to a query.
--- This allows your program to share the state
--- between repl iterations.
--- Right contains an optional message to print and
--- an updated program's state.
-stateTransition :: State -> Query -> Either String (Maybe String, State)
-stateTransition _ _ = Left "Not implemented 3"
+-- Utility to apply to parsers and get either first or second
+or2 :: Parser a -> Parser a -> Parser a
+or2 a b input
+  = case a input of
+      Right r1 -> Right r1
+      Left e1
+        -> case b input of
+             Right r2 -> Right r2
+             Left e2 -> Left (e1 ++ ", " ++ e2)
+
+-- Parser for a whole query
+parseQuery :: Parser Query
+parseQuery input =
+    case words input of
+      ("add-movie" : rest) -> parseAddMovie (unwords rest)
+      ("remove-movie" : rest) -> parseRemoveMovie (unwords rest)
+      ("print-movies" : _) -> Right (PrintMovies, "")
+      ("compound-query" : rest) -> parseCompoundQuery (unwords rest)
+      _ -> Left "Invalid command."
+
+-- Parser for add-movie
+parseAddMovie :: Parser Query
+parseAddMovie input =
+    let (_, afterSlash) = break (== '/') input
+    in case wordsBy ('/' ==) afterSlash of
+        [d, t, y, i] ->
+            case (parseNum y, parseNum i) of
+                (Right (y', _), Right (idNum, _)) ->
+                    Right (AddMovie d t y' idNum, "")
+                _ -> Left "Invalid year or ID in add-movie"
+        _ -> Left "Invalid add-movie input format"
+
+-- Parser for remove-movie
+parseRemoveMovie :: Parser Query
+parseRemoveMovie input =
+    case parseNum input of
+        Right (idNum, _) -> Right (RemoveMovie idNum, "")
+        _ -> Left "Invalid remove-movie input format"
+
+-- Parser for compound-query
+parseCompoundQuery :: Parser Query
+parseCompoundQuery input =
+    let (q1Str, r) = break (== '&') input
+    in case r of
+        '&' : q2Str ->
+            case (parseQuery q1Str, parseQuery q2Str) of
+                (Right (q1, _), Right (q2, _)) -> Right (CompoundQuery q1 q2, "")
+                _ -> Left "Invalid subqueries in compound-query"
+        _ -> Left "Invalid compound-query format"
+
+-- Helper function to parse a number
+parseNum :: Parser Integer
+parseNum input =
+    let digits = takeWhile isDigit input
+        rest = drop (length digits) input
+    in if null digits then Left "Expected a number" else Right (read digits, rest)
+
+-- Helper function to split by a given delimiter
+wordsBy :: (Char -> Bool) -> String -> [String]
+wordsBy p s =
+    case dropWhile p s of
+        "" -> []
+        s' -> w : wordsBy p s''
+              where (w, s'') = break p s'
+
+
+-- Updates the state according to a query.
+stateTransition :: State -> (Query, String) -> Either String (Maybe String, State)
+-- Handle AddMovie
+stateTransition (State movies) (AddMovie d t y i, _) =
+    let newMovie = Movie d t y i
+    in if newMovie `elem` movies
+       then Left ("Movie " ++ show newMovie ++ " already exists.")
+       else Right (Just ("Added movie: " ++ show newMovie), State (newMovie : movies))
+
+-- Handle RemoveMovie
+stateTransition (State movies) (RemoveMovie idNum, _) =
+    let movieExists = any (\(Movie _ _ _ id') -> id' == idNum) movies
+    in if movieExists
+       then Right (Just ("Removed movie with ID: " ++ show idNum), State (filter (\(Movie _ _ _ id') -> id' /= idNum) movies))
+       else Left ("Movie with ID " ++ show idNum ++ " not found.")
+
+-- Handle PrintMovies
+stateTransition (State movies) (PrintMovies, _) =
+    if null movies
+    then Right (Just "No movies available.", State movies)
+    else Right (Just ("Current movies: " ++ printMovies movies), State movies)
+
+-- Handle CompoundQuery
+stateTransition state (CompoundQuery q1 q2, a) =
+    case stateTransition state (q1, a) of
+        Right (msg1, newState) ->
+            case stateTransition newState (q2, a) of
+                Right (msg2, finalState) ->
+                    let combinedMsg = unwords $ filter (not . null) [maybe "" Prelude.id msg1, maybe "" Prelude.id msg2]
+                    in Right (Just combinedMsg, finalState)
+                Left err -> Left err
+        Left err -> Left err
+
+-- Add movie to the state
+addMovieTransition :: State -> String -> String -> Integer -> Integer -> Either String (Maybe String, State)
+addMovieTransition (State newState) d t y i =
+    let newMovie = addMovie newState (Movie d t y i)
+    in Right (Just "Movie added", State newMovie)
+
+-- Remove movie from the state
+removeMovieTransition :: State -> Integer -> Either String (Maybe String, State)
+removeMovieTransition (State newState) idNum =
+    let oldMovie = removeMovie newState idNum
+    in Right (Just "Movie removed", State oldMovie)
+
+-- Print movies in the state
+printMoviesTransition :: State -> Either String (Maybe String, State)
+printMoviesTransition (State newState) =
+    let printer = printMovies newState
+    in Right (Just printer, State newState)
+
+-- Helper to remove a movie from the list
+removeMovie :: [Movie] -> Integer -> [Movie]
+removeMovie [] _ = []
+removeMovie (Movie dir t y idNum : movies) n
+    | idNum == n = removeMovie movies n
+    | otherwise = Movie dir t y idNum : removeMovie movies n
+
+-- Helper to add a movie to the list
+addMovie :: [Movie] -> Movie -> [Movie]
+addMovie movies n = movies ++ [n]
+
+-- Helper to print a movie
+printMovie :: Movie -> String
+printMovie (Movie d t y i) =
+        "Director: " ++ d ++ ", Title: " ++ t ++
+        ", Year: " ++ show y ++ ", Id: " ++ show i
+
+-- Helper to print all movies
+printMovies :: [Movie] -> String
+printMovies [] = "No movies to display"
+printMovies movies = unlines $ map printMovie movies
+
+-- Sample movie list
+movie1 :: Movie
+movie1 = Movie "Pirmas" "Filmas#1" 1999 1
+
+movie2 :: Movie
+movie2 = Movie "Antras" "Filmas#2" 2000 2
+
+movie3 :: Movie
+movie3 = Movie "Trecias" "Filmas#3" 2004 3
+
+movieList :: [Movie]
+movieList = [movie1, movie2, movie3]
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,7 @@
 {-# LANGUAGE ImportQualifiedPost #-}
-import Test.Tasty ( TestTree, defaultMain, testGroup )
-import Test.Tasty.HUnit ( testCase, (@?=) )
-
-import Lib1 qualified
+import Test.Tasty (TestTree, defaultMain, testGroup)
+import Test.Tasty.HUnit (testCase, (@?=))
+--import Lib1 qualified
 import Lib2 qualified
 
 main :: IO ()
@@ -11,12 +10,165 @@ main = defaultMain tests
 tests :: TestTree
 tests = testGroup "Tests" [unitTests]
 
+-- unitTests :: TestTree
+-- unitTests = testGroup "Lib1 tests"
+--   [ 
+--     testCase "List of completions is not empty" $
+--       null Lib1.completions @?= False,
+--     testCase "Parsing case 1 - give a better name" $
+--       Lib2.parseQuery "" @?= Left "Invalid command.",
+--     testCase "Parsing case 2 - give a better name" $
+--       Lib2.parseQuery "o" @?= Left "Invalid command."
+--   ]
+
 unitTests :: TestTree
-unitTests = testGroup "Lib1 tests"
-  [ testCase "List of completions is not empty" $
-      null Lib1.completions @?= False,
-    testCase "Parsing case 1 - give a better name" $
-      Lib2.parseQuery "" @?= (Left "Some error message"),
-    testCase "Parsing case 2 - give a better name" $
-      Lib2.parseQuery "o" @?= (Left "Some error message")
+unitTests = testGroup "Lib2 tests"
+  [ 
+     -- Tests for parseQuery
+    testCase "Parse valid add-movie command" $
+      Lib2.parseQuery "add-movie /Tom/Showdown/2022/100" @?=
+        Right (Lib2.AddMovie "Tom" "Showdown" 2022 100, ""),
+
+    testCase "Parse valid add-movie command with extra spaces" $
+      Lib2.parseQuery "add-movie       /Tom/Showdown/2022/100" @?=
+        Right (Lib2.AddMovie "Tom" "Showdown" 2022 100, ""),
+
+    testCase "Parse valid remove-movie command" $
+      Lib2.parseQuery "remove-movie 100" @?=
+        Right (Lib2.RemoveMovie 100, ""),
+
+    testCase "Parse print-movies command" $
+      Lib2.parseQuery "print-movies" @?=
+        Right (Lib2.PrintMovies, ""),
+
+    testCase "Parse valid compound-query command" $
+      Lib2.parseQuery "compound-query add-movie /Tom/Showdown/2022/100 & remove-movie 100" @?=
+        Right (Lib2.CompoundQuery
+                (Lib2.AddMovie "Tom" "Showdown" 2022 100)
+                (Lib2.RemoveMovie 100), ""),
+
+    testCase "Parse compound-query with spaces" $
+      Lib2.parseQuery "compound-query  add-movie /Tom/Showdown/2022/100  &  remove-movie 100" @?=
+        Right (Lib2.CompoundQuery
+                (Lib2.AddMovie "Tom" "Showdown" 2022 100)
+                (Lib2.RemoveMovie 100), ""),
+
+    testCase "Invalid command type" $
+      Lib2.parseQuery "add-moviex /Tom/Showdown/2022/100" @?=
+        Left "Invalid command.",
+
+    testCase "Invalid add-movie format" $
+      Lib2.parseQuery "add-movie /Tom/Showdown/2022" @?=
+        Left "Invalid add-movie input format",
+
+    testCase "Invalid remove-movie format" $
+      Lib2.parseQuery "remove-movie notANumber" @?=
+        Left "Invalid remove-movie input format",
+
+    testCase "Invalid compound-query format" $
+      Lib2.parseQuery "compound-query add-movie /Tom/Showdown/2022/100 &" @?=
+        Left "Invalid compound-query format",
+
+    testCase "Empty input" $
+      Lib2.parseQuery "" @?=
+        Left "Invalid command.",
+
+    -- stateTransition
+    testCase "State transition with AddMovie" $
+      let initialState = Lib2.emptyState
+          query = Lib2.AddMovie "Tom" "Showdown" 2022 100
+          expectedState = Lib2.State [Lib2.Movie "Tom" "Showdown" 2022 100]
+      in stateTransition initialState (query, "") @?=
+        Right (Just "Added movie: Movie {director = \"Tom\", title = \"Showdown\", year = 2022, id = 100}", expectedState),
+
+    testCase "State transition with RemoveMovie" $
+      let initialState = Lib2.State [Lib2.Movie "Tom" "Showdown" 2022 100]
+          query = Lib2.RemoveMovie 100
+      in stateTransition initialState (query, "") @?= Right (Just "Removed movie with ID: 100", Lib2.State [])
+
+    testCase "State transition with PrintMovies" $
+      let initialState = Lib2.State [Lib2.Movie "Tom" "Showdown" 2022 100]
+          query = Lib2.PrintMovies
+      in stateTransition initialState (query, "") @?= Right (Just "Current movies: Director: Tom, Title: Showdown, Year: 2022, Id: 100\n", initialState),
+
+    testCase "CompoundQuery with two valid queries" $
+      let initialState = Lib2.emptyState
+          query = Lib2.CompoundQuery (Lib2.AddMovie "Tom" "Showdown" 2022 100) Lib2.PrintMovies
+          expectedState = Lib2.State [Lib2.Movie "Tom" "Showdown" 2022 100]
+      in stateTransition initialState (query, "") @?= Right (Just "Added movie: Movie {director = \"Tom\", title = \"Showdown\", year = 2022, id = 100} Current movies: Director: Tom, Title: Showdown, Year: 2022, Id: 100\n"), expectedState,
+
+    -- parseAddMovie
+    testCase "Parse valid AddMovie input" $
+      Lib2.parseAddMovie "/Tom/Showdown/2022/100" @?= 
+        Right (Lib2.AddMovie "Tom" "Showdown" 2022 100, ""),
+
+    testCase "Parse invalid AddMovie input" $
+      Lib2.parseAddMovie "/Tom/Showdown/invalid_year/100" @?= 
+        Left "Invalid year or ID in add-movie",
+
+    -- parseRemoveMovie
+    testCase "Parse valid RemoveMovie input" $
+      Lib2.parseRemoveMovie "100" @?= 
+        Right (Lib2.RemoveMovie 100, ""),
+
+    testCase "Parse invalid RemoveMovie input" $
+      Lib2.parseRemoveMovie "abc" @?= 
+        Left "Invalid remove-movie input format",
+
+    -- parseCompoundQuery
+    testCase "Parse valid CompoundQuery" $
+      Lib2.parseCompoundQuery "add-movie /Tom/Showdown/2022/100 & print-movies" @?= 
+        Right (Lib2.CompoundQuery (Lib2.AddMovie "Tom" "Showdown" 2022 100) Lib2.PrintMovies, ""),
+
+    testCase "Parse invalid CompoundQuery" $
+      Lib2.parseCompoundQuery "invalid_query & print-movies" @?= 
+        Left "Invalid subqueries in compound-query",
+
+    -- parseNum
+    testCase "Parse valid number" $
+      Lib2.parseNum "100 more" @?= Right (100, " more"),
+
+    testCase "Parse invalid number" $
+      Lib2.parseNum "abc more" @?= Left "Expected a number",
+
+    -- wordsBy
+    testCase "WordsBy delimiter test" $
+      Lib2.wordsBy ('/' ==) "Hello/World/Example" @?= ["Hello", "World", "Example"],
+
+    testCase "WordsBy no delimiter test" $
+      Lib2.wordsBy ('/' ==) "NoDelimitersHere" @?= ["NoDelimitersHere"],
+
+    -- addMovie
+    testCase "Add a movie to an empty list" $
+      let movies = []
+          newMovie = Lib2.Movie "Tom" "Showdown" 2022 100
+          updatedMovies = Lib2.addMovie movies newMovie
+      in updatedMovies @?= [newMovie],
+
+    testCase "Add a movie to a non-empty list" $
+      let movies = [Lib2.Movie "Tom" "Showdown" 2022 100]
+          newMovie = Lib2.Movie "Jane" "Epic" 2023 101
+          updatedMovies = Lib2.addMovie movies newMovie
+      in updatedMovies @?= [Lib2.Movie "Tom" "Showdown" 2022 100, newMovie],
+
+    -- removeMovie
+    testCase "Remove a movie that exists" $
+      let movies = [Lib2.Movie "Tom" "Showdown" 2022 100]
+          updatedMovies = Lib2.removeMovie movies 100
+      in updatedMovies @?= [],
+
+    testCase "Remove a movie that does not exist" $
+      let movies = [Lib2.Movie "Tom" "Showdown" 2022 100]
+          updatedMovies = Lib2.removeMovie movies 999
+      in updatedMovies @?= [Lib2.Movie "Tom" "Showdown" 2022 100], 
+
+    -- printMovie
+    testCase "Print a movie" $
+      let movie = Lib2.Movie "Tom" "Showdown" 2022 100
+      in Lib2.printMovie movie @?= "Director: Tom, Title: Showdown, Year: 2022, Id: 100",
+
+    -- printMovies
+    testCase "Print movies" $
+      let movies = [Lib2.Movie "Tom" "Showdown" 2022 100, Lib2.Movie "Jane" "Epic" 2023 101]
+      in Lib2.printMovies movies @?= "Director: Tom, Title: Showdown, Year: 2022, Id: 100\nDirector: Jane, Title: Epic, Year: 2023, Id: 101\n"
   ]
